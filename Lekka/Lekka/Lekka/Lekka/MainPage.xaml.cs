7490c666b488343337115591b6e9ce22a967be5e
Raghu <raghu1986@outlook.com>
2017-06-20 13:56:01 +0530

    Encryption and decription Written

--- Lekka/Lekka/Lekka/Lekka/MainPage.xaml.cs
+++ Lekka/Lekka/Lekka/Lekka/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Lekka.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,9 @@ namespace Lekka
         public MainPage()
         {
             this.InitializeComponent();
+
+            Txt2.Text= Crypto.Encrypt(Txt1.Text.ToString());
+            Txt3.Text = Crypto.Decrypt(Txt2.Text.ToString());
         }
     }
 }
---
+++ Lekka/Lekka/Lekka/Lekka/Models/Crypto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+
+namespace Lekka.Models
+{
+    public class Crypto
+    {
+        private static byte[] Values = { 1,4,8,15,56,213,42,108, 1, 4, 18, 15, 16, 23, 142, 108,
+                                         1,24,8,15,16,213,42,108, 1, 24, 8, 15, 16, 213, 42, 108};
+        private static SymmetricKeyAlgorithmProvider Algorithm = SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmNames.AesCbcPkcs7);
+
+        private static CryptographicKey Ckey = Algorithm.CreateSymmetricKey(CryptographicBuffer.CreateFromByteArray(Values));
+
+        private static IBuffer Ib = Values.AsBuffer();
+
+
+        public static string Encrypt(string RawData)
+        {
+            string Encrp = "";
+            try
+            {
+                IBuffer buffer = CryptographicBuffer.ConvertStringToBinary(RawData, BinaryStringEncoding.Utf8);
+                IBuffer bufferEncrypt = CryptographicEngine.Encrypt(Ckey, buffer, Ib);
+                Encrp = CryptographicBuffer.EncodeToBase64String(bufferEncrypt);
+            }
+            catch
+            {
+                Encrp = "error!";
+            }
+
+            return Encrp;
+            
+        }
+
+        public static string Decrypt(string EncrpData)
+        {
+            string Decrp = "";
+            try
+            {
+                IBuffer buffer =Convert.FromBase64String(EncrpData).AsBuffer();
+                IBuffer bufferDecrypt = CryptographicEngine.Decrypt(Ckey, buffer, Ib);
+                Decrp = CryptographicBuffer.ConvertBinaryToString(BinaryStringEncoding.Utf8,bufferDecrypt);
+            }
+            catch
+            {
+                Decrp = "error!";
+            }
+
+            return Decrp;
+
+        }
+        
+
+    }
+}
